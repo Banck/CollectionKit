0e15c8a20a3789dbee2aebe0d10078473e6e91ae
Egor Sakhabaev <sakhabaevegor@gmail.com>
2021-12-13 12:48:41 +0700

    Add HashableSizeSource

---
+++ Sources/SizeSource/HashableSizeSource.swift
@@ -0,0 +1,23 @@
+//
+//  HashableDataSource.swift
+//  CollectionKit
+//
+//  Created by Egor Sakhabaev on 13.12.2021.
+//  Copyright © 2021 lkzhao. All rights reserved.
+//
+
+import Foundation
+
+open class HashableSizeSource<Data: Hashable>: ClosureSizeSource<Data> {
+    private var hashedSizes: [AnyHashable: CGSize] = [:]
+    
+    open override func size(at index: Int, data: Data, collectionSize: CGSize) -> CGSize {
+        if let size = hashedSizes[data] {
+            return size
+        } else {
+            let size = super.size(at: index, data: data, collectionSize: collectionSize)
+            hashedSizes[data] = size
+            return size
+        }
+    }
+}
